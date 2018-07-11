0ff7d2f724acf4ca00ba3c0477bfb9403b07cbc4
E Jun <jason_e@syscom.com.cn>
2018-07-11 10:26:11 +0800

    use gridLayout to choose mask

--- app/src/main/java/com/wecyberstage/wecyberstage/view/helper/PopupChooseMask.java
+++ app/src/main/java/com/wecyberstage/wecyberstage/view/helper/PopupChooseMask.java
@@ -4,11 +4,15 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
 import com.wecyberstage.wecyberstage.R;
@@ -32,11 +36,26 @@ public class PopupChooseMask extends PopupWindow{
         this.maskGraph = maskGraph;
         View contentView = LayoutInflater.from(view.getContext()).inflate(R.layout.popup_mask_choose,null);
         ButterKnife.bind(this, contentView);
+        if(mask.maskGraphList.size() <= 4) {
+            ((GridLayout) masksFrame).setColumnCount(2);
+            ((GridLayout) masksFrame).setRowCount(2);
+        } else if(mask.maskGraphList.size() <= 9) {
+            ((GridLayout) masksFrame).setColumnCount(3);
+            ((GridLayout) masksFrame).setRowCount(3);
+        } else if(mask.maskGraphList.size() <= 16) {
+            ((GridLayout) masksFrame).setColumnCount(4);
+            ((GridLayout) masksFrame).setRowCount(4);
+        }
         // add mask to content view
         for(MaskGraph mg: mask.maskGraphList) {
             Log.i("PopupChooseMask","maskGraph ID: " + mg.id);
+            int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, view.getContext().getResources().getDisplayMetrics());
+            int width =  (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, view.getContext().getResources().getDisplayMetrics());
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(height, width);
             ImageButton imageButton = new ImageButton(view.getContext());
-            masksFrame.addView(imageButton);
+            imageButton.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            // imageButton.setId(R.id.image_view_1);
+            masksFrame.addView(imageButton, layoutParams);
             Glide.with(view.getContext()).load(mg.graphURL).into(imageButton);
         }
         setContentView(contentView);
