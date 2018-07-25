61badbc8bb68984108d967cfc74b21cbb2a46dda
E Jun <jason_e@syscom.com.cn>
2018-07-25 11:02:02 +0800

    add ic_stop ic_volume ic_pause icon

--- app/src/main/java/com/wecyberstage/wecyberstage/view/main/PlayerControlBar.java
+++ app/src/main/java/com/wecyberstage/wecyberstage/view/main/PlayerControlBar.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
-public class PlayerControlBar extends LinearLayout {
+public class PlayerControlBar extends RelativeLayout {
 
     public PlayerControlBar(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
+
+
 
 }
